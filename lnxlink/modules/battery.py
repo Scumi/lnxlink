291c48b3e3e484a7589824c7b0f81cbd23e82567
Stefan Schumann <stefan.schumann@posteo.de>
2024-12-01 09:28:23 +0100

    modules/battery.py: remove obsolete .replace()

--- lnxlink/modules/battery.py
+++ lnxlink/modules/battery.py
@@ -55,7 +55,7 @@ class Addon:
                     [
                         device["Vendor"],
                         device["Model"],
-                        device["Serial"].replace(":", ""),
+                        device["Serial"]
                     ]
                 )
                 .strip()
